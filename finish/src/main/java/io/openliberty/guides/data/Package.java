46553512136377fe0e7434adb166ff31241ffada
Mark Swatosh <mark.swatosh@gmail.com>
2024-08-22 12:28:21 -0500

    Add callQuery and Package toJson

--- finish/src/main/java/io/openliberty/guides/data/Package.java
+++ finish/src/main/java/io/openliberty/guides/data/Package.java
@@ -1,5 +1,16 @@
 package io.openliberty.guides.data;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObjectBuilder;
+
 public record Package(int id, float length, float width, float height) {
     
+    JsonObjectBuilder toJson() {
+        JsonObjectBuilder json = Json.createObjectBuilder();
+        json.add("id", id);
+        json.add("length", length);
+        json.add("width", width);
+        json.add("height", height);
+        return json;
+    }
 }
--- finish/src/main/java/io/openliberty/guides/data/PackageQueryService.java
+++ finish/src/main/java/io/openliberty/guides/data/PackageQueryService.java
@@ -1,19 +1,29 @@
 package io.openliberty.guides.data;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.stream.Stream;
 
+import jakarta.inject.Inject;
 import jakarta.json.Json;
 import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
 import jakarta.json.JsonObjectBuilder;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/packageQuery")
 public class PackageQueryService {
-    
+
+    @Inject
+    Packages packages;
+
+    //TODO ignore queries that require an Object as a parameter e.g. delete requires an Entity
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -36,5 +46,28 @@ public class PackageQueryService {
 
         return queryList.build().toString();
 
+    }
+
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String callQuery(JsonObject json) {
+        try {
+            Method method = Packages.class.getMethod(json.getString("method"));
+            Object result = method.invoke(packages);
+
+            JsonArrayBuilder returnList = Json.createArrayBuilder();
+            if (result instanceof Stream) {
+                ((Stream<?>)result).forEach(p -> {
+                    returnList.add(((Package) p).toJson());
+                });
+            }
+            return returnList.toString();
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            // TODO Reply to Client with error message
+            e.printStackTrace();
+        }
+        return "";
     }
 }
